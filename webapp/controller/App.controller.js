80466836ceed9a839d4fb1001804d726c88410c0
kotha32 <sumanthkotha32@gmail.com>
2024-03-26 17:27:03 +0530

    OData--V4

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/m/MessageBox",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, MessageBox, JSONModel) {
+	"use strict";
+
+	return Controller.extend("sap.ui.core.tutorial.odatav4.controller.App", {
+
+		/**
+		 *  Hook for initializing the controller
+		 */
+		onInit : function () {
+			var oJSONData = {
+					busy : false
+				},
+				oModel = new JSONModel(oJSONData);
+
+			this.getView().setModel(oModel, "appView");
+		},
+
+
+	});
+});
