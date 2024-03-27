90df442188e97b4ed7b874c46b0d0265bd4807e8
kotha32 <sumanthkotha32@gmail.com>
2024-03-27 12:35:00 +0530

    changes done

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -8,18 +8,30 @@ sap.ui.define([
 
 	return Controller.extend("sap.ui.core.tutorial.odatav4.controller.App", {
 
-		/**
-		 *  Hook for initializing the controller
-		 */
 		onInit : function () {
 			var oJSONData = {
-					busy : false
-				},
-				oModel = new JSONModel(oJSONData);
-
+				busy : false
+			};
+			var oModel = new JSONModel(oJSONData);
 			this.getView().setModel(oModel, "appView");
 		},
 
+		onRefresh : function () {
+			var oBinding = this.byId("productList").getBinding("items");
 
+			if (oBinding.hasPendingChanges()) {
+				MessageBox.error(this._getText("refreshNotPossibleMessage"));
+				return;
+			}
+			oBinding.refresh();
+			MessageToast.show(this._getText("refreshSuccessMessage"));
+		},
+
+		_getText : function (sTextId, aArgs) {
+			return this.getOwnerComponent().getModel("i18n").getResourceBundle().getText(sTextId, aArgs);
+
+		}
 	});
 });
+
+
